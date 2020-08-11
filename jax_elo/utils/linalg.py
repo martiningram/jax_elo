8bf662987a054f9c2e709815ffec452c3d74eb0b
Martin Ingram <ingramm@student.unimelb.edu.au>
2020-08-11 15:51:48 +1000

    Added missing linear algebra utils

---
+++ jax_elo/utils/linalg.py
@@ -0,0 +1,36 @@
+import jax.numpy as jnp
+from jax.ops import index_update
+
+
+def pos_def_mat_from_tri_elts(elts, mat_size, jitter=1e-6):
+
+    cov_mat = lo_tri_from_elements(elts, mat_size)
+    cov_mat = cov_mat @ cov_mat.T
+
+    cov_mat = cov_mat + jnp.eye(mat_size) * jitter
+
+    return cov_mat
+
+
+def num_mat_elts(num_triangular_elts):
+
+    sqrt_term = jnp.sqrt(8 * num_triangular_elts + 1)
+
+    return int((sqrt_term - 1) / 2)
+
+
+def num_triangular_elts(mat_size, include_diagonal=True):
+
+    if include_diagonal:
+        return int(mat_size * (mat_size + 1) / 2)
+    else:
+        return int(mat_size * (mat_size - 1) / 2)
+
+
+def lo_tri_from_elements(elements, n):
+
+    L = jnp.zeros((n, n))
+    indices = jnp.tril_indices(L.shape[0])
+    L = index_update(L, indices, elements)
+
+    return L
