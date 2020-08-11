577cd9f022c06b7e281a2835d52d783d32c932de
Martin Ingram <martin.ingram@gmail.com>
2020-08-11 15:59:56 +1000

    Added missing import

--- jax_elo/elo_functions/basic.py
+++ jax_elo/elo_functions/basic.py
@@ -8,10 +8,10 @@ from jax.scipy.stats import multivariate_normal
 from jax_elo.utils.normals import weighted_sum, logistic_normal_integral_approx
 from jax_elo.core import EloFunctions, calculate_win_prob
 from jax_elo.utils.flattening import reconstruct
-from jax_elo.utils.linalg import pos_def_mat_from_tri_elts
+from jax_elo.utils.linalg import pos_def_mat_from_tri_elts, num_mat_elts
 
 
-b = jnp.log(10) / 400.
+b = jnp.log(10) / 400.0
 
 
 @jit
@@ -27,8 +27,9 @@ def calculate_marginal_lik(x, mu, a, cov_mat, theta, y):
 
     latent_mean, latent_var = weighted_sum(x, cov_mat, a)
 
-    win_prob = jnp.log(logistic_normal_integral_approx(
-        b * latent_mean, b**2 * latent_var))
+    win_prob = jnp.log(
+        logistic_normal_integral_approx(b * latent_mean, b ** 2 * latent_var)
+    )
 
     return win_prob
 
@@ -42,15 +43,16 @@ def calculate_prior(x, mu, cov_mat, theta):
 @jit
 def calculate_log_posterior(x, mu, cov_mat, a, theta, y):
 
-    return (calculate_likelihood(x, mu, a, theta, y) +
-            calculate_prior(x, mu, cov_mat, theta))
+    return calculate_likelihood(x, mu, a, theta, y) + calculate_prior(
+        x, mu, cov_mat, theta
+    )
 
 
 def parse_theta(flat_theta, summary):
 
     theta = reconstruct(flat_theta, summary, jnp.reshape)
-    n_elts = num_mat_elts(len(theta['cov_mat']))
-    theta['cov_mat'] = pos_def_mat_from_tri_elts(theta['cov_mat'], n_elts)
+    n_elts = num_mat_elts(len(theta["cov_mat"]))
+    theta["cov_mat"] = pos_def_mat_from_tri_elts(theta["cov_mat"], n_elts)
 
     return theta
 
@@ -60,4 +62,5 @@ basic_functions = EloFunctions(
     log_post_hess_x=jit(hessian(calculate_log_posterior)),
     marginal_lik_fun=calculate_marginal_lik,
     parse_theta_fun=parse_theta,
-    win_prob_fun=jit(partial(calculate_win_prob, pre_factor=b)))
+    win_prob_fun=jit(partial(calculate_win_prob, pre_factor=b)),
+)
